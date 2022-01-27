2b0a29447d7c13e1256e28d71447f63c73c729fd
wmxkb <54021527+wmxkb@users.noreply.github.com>
2022-01-27 10:32:54 +0800

    测试

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-console.error("电脑炸了");
+// console.error("电脑炸了");
 
-//例一
-for (var i = 0; i < 10; i++) {
-    setTimeout(function(){
-        console.log(i);
-    },100)
-};
-//例二
-for (let i = 0; i < 10; i++) {
-    setTimeout(function(){
-        console.log(i);
-    },100)
-};
+// //例一
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(function(){
+//         console.log(i);
+//     },100)
+// };
+// //例二
+// for (let i = 0; i < 10; i++) {
+//     setTimeout(function(){
+//         console.log(i);
+//     },100)
+// };
+
+var a = 3
+var b = 4
+var c = a+b
+// (a + b).toString
+
+// console.log()
+
